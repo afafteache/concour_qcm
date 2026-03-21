7da64fc0856611615c0c72a1780a69b0e1dddf94
Salma-prog-pixel <salmazouhri306@gmail.com>
2026-03-21 20:15:49 +0000

    Toutes les modifications

--- setup.py
+++ setup.py
@@ -20,10 +20,11 @@ print("✅ Admin : Salma Ben Aouama — CIN: OD67857 — MDP: admin123")
 ens = User.objects.create_user(
     username='OD67853',
     password='ens123',
-    first_name='Fatiha',
-    last_name='Zouhri',
+    first_name='Afaf',
+    last_name='Teache',
+     email='afafteache@gmail.com',
 )
 Profil.objects.create(user=ens, cin='OD67853', role='enseignant')
-print("✅ Enseignant : Fatiha Zouhri — CIN: OD67853 — MDP: ens123")
+print("✅ Enseignant : Afaf Teache — CIN: OD67853 — MDP: ens123")
 
 print("\n🎉 Setup terminé !")
